c85d2a7cb83277f0e63ec91be2cc8a614db3021a
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-05-19 23:36:22 +0200

    handle ckeditor issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -15,5 +16,8 @@
 //= require ckeditor-jquery
 //= require popper
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
+
+$('.ckeditor').ckeditor({
+    // optional config
+});
